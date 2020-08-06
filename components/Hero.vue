9daf1cc0af00fc693c97244f9b5a64e8ecceafa9
NessMC <contact@nessmc.fr>
2020-08-06 20:08:20 +0200

    🍱 Sample profile picture added.

--- components/Hero.vue
+++ components/Hero.vue
@@ -20,7 +20,7 @@ export default Vue.extend({
 .nessmc\:hero {
   position: absolute;
   top: 50%;
-  left: calc(100% - 790px);
+  left: calc(100% - 792px);
   transform: translate(-50%, -50%);
   height: 50%;
   min-height: unset !important;
@@ -41,6 +41,7 @@ export default Vue.extend({
     display: flex;
     flex: 1 0 65%;
     background: blue;
+    border-radius: 25px 0 0 25px;
     @media (max-width: 1440px) {
       flex: 1 0 50%;
     }
@@ -52,7 +53,11 @@ export default Vue.extend({
     min-height: unset !important;
     display: flex;
     flex: 1 0 35%;
-    background: red;
+    background-image: url('../assets/logo.jpg');
+    background-position: center center;
+    background-size: cover;
+    filter: grayscale(100%);
+    border-radius: 0 25px 25px 0;
     @media (max-width: 1440px) {
       flex: 1 0 50%;
     }
