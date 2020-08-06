57b9eea488cd943106eda172cff82a49659ba6c5
NessMC <contact@nessmc.fr>
2020-08-06 17:38:18 +0200

    🎉 Portfolio project created.

---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <Header>
+    <Navbar />
+    <Hero />
+  </Header>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import Header from '~/components/Header.vue'
+import Navbar from '~/components/Navbar.vue'
+import Hero from '~/components/Hero.vue'
+
+export default Vue.extend({
+  components: {
+    Header,
+    Navbar
+  }
+})
+</script>
