0f8ab0a83de071c3d7fe4eb62486a026d3b8e2ed
NessMC <contact@nessmc.fr>
2020-08-17 15:30:12 +0200

    ✨ Header component added.

--- components/Header.vue
+++ components/Header.vue
@@ -1,14 +1,10 @@
 <template>
-  <Header />
+  
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
-import Header from '@/components/Header.vue'
-
 export default Vue.extend({
-  components: {
-    Header
-  }
+  
 })
 </script>
