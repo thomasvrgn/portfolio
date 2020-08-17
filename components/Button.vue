358b6be939bc7681d74ed266b013a6dd24692512
NessMC <contact@nessmc.fr>
2020-08-17 15:59:59 +0200

    ✨ Button component added.

---
+++ components/Button.vue
@@ -0,0 +1,15 @@
+<template>
+  <a href="" class="my-1 mx-2 py-2 px-6 rounded text-white font-semibold transition duration-300" :class="`bg-${color}-500 hover:bg-${color}-600 focus:bg-${color}-700`">
+    {{ content }}
+  </a>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  props: {
+    color: String,
+    content: String
+  }
+})
+</script>
--- components/Header.vue
+++ components/Header.vue
@@ -1,10 +1,43 @@
 <template>
-  
+  <div class="bg-gray-200 h-screen flex flex-row">
+    <div class="flex flex-auto lg:w-3/4 lg:bg-red-200">
+      <div class="flex flex-auto justify-center items-center">
+        <div class="flex flex-initial justify-center items-center bg-white shadow-lg w-2/3 rounded-lg">
+          <div class="container p-8 flex flex-row">
+            <div class="flex flex-auto items-center">
+              <div class="container">
+                <h1 class="font-bold text-3xl text-gray-800">
+                  Ness
+                </h1>
+                <p class="italic text-gray-600">
+                  Web developer
+                </p>
+              </div>
+            </div>
+            <div class="flex flex-auto w-3/4 items-center">
+              <div class="container flex flex-row flex-wrap justify-center">
+                <Button content="Button" color="red" />
+                <Button content="Button" color="purple" />
+                <Button content="Button" color="blue" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="flex flex-auto lg:w-1/4 bg-blue-400 hidden">
+
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
+import Button from './Button.vue'
+
 export default Vue.extend({
-  
+  components: {
+    Button
+  }
 })
 </script>
